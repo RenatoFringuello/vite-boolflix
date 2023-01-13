d56ce8d116cc0413067958d0dd16b78dbbdf0006
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-13 19:03:14 +0100

    add a flag to loading data

--- src/store.js
+++ src/store.js
@@ -2,16 +2,18 @@ import { reactive } from "vue"
 import axios from 'axios'
 
 export const store = reactive({
+    apiUrl:'https://api.themoviedb.org/3/search/',
+    apiKey:'12faac57c6ce385924debc3b88eae9d3',
     dataRaw : [],
     filmList : [''],
     nPage : 0,
-    apiUrl:'https://api.themoviedb.org/3/search/',
-    apiKey:'12faac57c6ce385924debc3b88eae9d3',
+    isLoaded:false,
     /**
      * 
      * @param {*} location movie | tv
      */
     getData(location, query){
+        this.isLoaded = false;
         //usa postman ci sono problemi
         axios.get(this.apiUrl + location, {params:{
             api_key: this.apiKey,
@@ -27,6 +29,7 @@ export const store = reactive({
             //do something after load data
             this.filmList = this.dataRaw.results;
             this.nPage = this.dataRaw.page;
+            this.isLoaded = true;
         })
     }
 });
