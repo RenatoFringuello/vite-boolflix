caf2282070e35f83ee11a61d969f74eb10ea0aa2
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-14 15:14:15 +0100

    assemble filmList and the loaders in a single obj

--- src/store.js
+++ src/store.js
@@ -5,16 +5,20 @@ export const store = reactive({
     apiUrl:'https://api.themoviedb.org/3/search/',
     apiKey:'12faac57c6ce385924debc3b88eae9d3',
     dataRaw : [],
-    filmList : [''],
-    nPage : 0,
-    isLoaded:false,
+    filmData : {data:[],isLoaded:false,isImgLoaded:false, currentPage:1},
+    tvData : {data:[],isLoaded:false,isImgLoaded:false, currentPage:1},
     /**
      * 
      * @param {*} location movie | tv
      */
     getData(location, query){
-        this.isLoaded = false;
-        //usa postman ci sono problemi
+        if(location === 'movie'){
+            this.filmData = {data:[],isLoaded:false,isImgLoaded:false, currentPage:1};
+        }
+        else{
+            this.tvData = {data:[],isLoaded:false,isImgLoaded:false, currentPage:1};        
+        }
+        
         axios.get(this.apiUrl + location, {params:{
             api_key: this.apiKey,
             query : query
@@ -26,10 +30,20 @@ export const store = reactive({
             console.error('something went wrong :/\n', error);
         })
         .then(()=>{
+            console.log(this.dataRaw)
             //do something after load data
-            this.filmList = this.dataRaw.results;
-            this.nPage = this.dataRaw.page;
-            this.isLoaded = true;
+            const requestObj = {
+                data: this.dataRaw.results,
+                isLoaded : true,
+                isImgLoaded: this.filmData.isImgLoaded,
+                currentPage : this.dataRaw.page
+            };
+            if(location === 'movie'){
+                this.filmData = requestObj;
+            }
+            else{
+                this.tvData = requestObj;
+            }
         })
     }
 });
