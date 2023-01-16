3547d7ff616d4e60618c2178376548d4e034253d
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-16 09:34:04 +0100

    cnage getData for configuration get

--- src/store.js
+++ src/store.js
@@ -2,47 +2,64 @@ import { reactive } from "vue"
 import axios from 'axios'
 
 export const store = reactive({
-    apiUrl:'https://api.themoviedb.org/3/search/',
+    // api 
+    apiUrl:'https://api.themoviedb.org/3/',
     apiKey:'12faac57c6ce385924debc3b88eae9d3',
+    // raw response
     dataRaw : [],
+    //configuration
+    configData:[],
+    //search/movie|tv
     filmData : {data:[],isLoaded:false,isImgLoaded:false, currentPage:1},
     tvData : {data:[],isLoaded:false,isImgLoaded:false, currentPage:1},
+    
     /**
      * 
      * @param {*} location movie | tv
      */
     getData(location, query){
-        if(location === 'movie'){
+        //reset movie or tv object
+        if(location === 'search/movie'){
             this.filmData = {data:[],isLoaded:false,isImgLoaded:false, currentPage:1};
         }
         else{
             this.tvData = {data:[],isLoaded:false,isImgLoaded:false, currentPage:1};        
         }
-        
+        //get call 
         axios.get(this.apiUrl + location, {params:{
             api_key: this.apiKey,
-            query : query
+            //if there isn' a query = if it's configuration
+            ...(query != '')&& {query : query}
         }})
         .then((result)=>{
+            //save raw data
             store.dataRaw = result.data;
         })
         .catch((error)=>{
             console.error('something went wrong :/\n', error);
         })
         .then(()=>{
-            console.log(this.dataRaw)
             //do something after load data
+            console.log(this.dataRaw);
+            //object to give to filmData | tvData
             const requestObj = {
                 data: this.dataRaw.results,
                 isLoaded : true,
                 isImgLoaded: this.filmData.isImgLoaded,
                 currentPage : this.dataRaw.page
             };
-            if(location === 'movie'){
-                this.filmData = requestObj;
-            }
-            else{
-                this.tvData = requestObj;
+            //do someting based on what is location
+            switch (location) {
+                case 'search/movie':
+                    this.filmData = requestObj;
+                    break;
+                case 'search/tv':
+                    this.tvData = requestObj;
+                    break;
+                case 'configuration':
+                    //get the configuration for the images base_url & sizes
+                    this.configData = this.dataRaw;
+                    break;
             }
         })
     }
